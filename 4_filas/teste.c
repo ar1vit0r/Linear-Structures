96efabaa20811b38e603e41a726069a3a8917382
Ari Vitor Lazzarotto <avdslazzarotto@inf.ufpel.edu.br>
2019-05-02 19:47:48 +0000

    tava com o teste da pilha ainda

--- 4_filas/teste.c
+++ 4_filas/teste.c
@@ -1,17 +1,17 @@
 #include "simplegrade.h"
-#include "pilha.h"
+#include "fila.h"
 
 #define MAX 5
 
 
 void test_create(){
-	DESCRIBE("CRIA PILHA");
-	WHEN("Eu crio uma pilha");
+	DESCRIBE("CRIA FILA");
+	WHEN("Eu crio uma FILA");
 
 
 	IF("Sem parâmetros");
 	THEN("Resultado deve ser ponteiro");
-	struct pilha * p = create();
+	struct fila * p = create();
 	isNotNull(p,1);
 
 	THEN("Deve ser vazia");
@@ -20,42 +20,40 @@ void test_create(){
 	destroy(p);
 }
 
-void test_push(){
+void test_queue(){
 	int i;
-	struct pilha * p = create();
+	struct fila * p = create();
 
 	DESCRIBE("INSERE ELEMENTOS");
 	WHEN("Eu insiro elementos");
 	IF("Insiro um número razoável MAX");
 	THEN("Deve inserir todos");
 	for(i=1; i<=MAX; i++){
-		isEqual(push(p,i), 1,1);
+		isEqual(enqueue(p,i), 1,1);
 	}
 	THEN("Deve não estar vazia");
 	isEqual(vazia(p),0,1);
 
-	THEN("Topo deve ser MAX");
-	isEqual(top(p),MAX,1);
+	THEN("Primeiro elemento deve ser 1");
+	isEqual(dequeue(p),1,1);
 
 	destroy(p);
 }
 
-void test_pop(){
+void test_dequeue(){
 
-	int i, val;
-	struct pilha * p = create();
+	int i;
+	struct fila * p = create();
 
 
 	DESCRIBE("REMOVE ELEMENTOS");
-	WHEN("Eu removo os elementos de uma pilha");
+	WHEN("Eu removo os elementos de uma FILA");
 	for(i=1; i<=MAX; i++){
-		push(p,i);
+		enqueue(p,i);
 	}
-	for(i=MAX; i>0; i--){
-		THEN("Deve ter topo igual a i");
-		isEqual(top(p),i,1);
-		THEN("Deve remover");
-		isEqual(pop(p),1,1);
+	for(i=1; i<=MAX; i++){
+		THEN("Deve ter cabeça igual a i");
+		isEqual(dequeue(p),i,1);
 	}
 
 	THEN("Deve ser vazia");
@@ -64,13 +62,30 @@ void test_pop(){
 }
 
 
+void test_makenull(){
+
+	int i;
+	struct fila * p = create();
+
+
+	DESCRIBE("REMOVE TODOS OS ELEMENTOS");
+	WHEN("Eu removo todos os elementos de uma FILA");
+	for(i=1; i<=MAX; i++){
+		enqueue(p,i);
+	}
+	makenull(p);
+	THEN("Deve ser vazia");
+	isEqual(vazia(p),1,1);
+	destroy(p);
+}
+
 
 int main () {
 	test_create();
-	test_push();
-	test_pop();
+	test_queue();
+	test_dequeue();
+	test_makenull();
 
-		
 	GRADEME();
 	
 	if (grade==maxgrade)
